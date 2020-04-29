1e110c024f1e99ddedea0aa6e2d3a20019b960f8
Joseph <jhl843@nyu.edu>
2020-04-29 22:42:39 +0900

    basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router, Redirect} from "react-router-dom";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+// Pages
+import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/create-account">
+          <CreateAccount />
+        </Route>
+      </Router>
     </div>
   );
 }
